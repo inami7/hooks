a3e5a1c6bded8445bfbc8963377eb3b23e5998c5
inami7 <103008905+inami7@users.noreply.github.com>
2022-04-08 17:54:04 +0900

    hooks-test

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,15 +1,21 @@
-import { VFC } from "react";
+import { FC } from "react";
+import { UserProfile } from "../types/userProfile";
 
-export const UserCard: VFC<Props> = () => {
+type Props = {
+  user: UserProfile;
+};
+
+export const UserCard: FC<Props> = (props) => {
+  const { user } = props;
   return (
     <div>
       <dl>
         <dt>名前</dt>
-        <dd></dd>
+        <dd>{user.name}</dd>
         <dt>メール</dt>
-        <dd></dd>
+        <dd>{user.email}</dd>
         <dt>住所</dt>
-        <dd></dd>
+        <dd>{user.address}</dd>
       </dl>
     </div>
   );
