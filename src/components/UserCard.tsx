f0fb1f94bce40722314a0ccc4d40ef8e8256266c
inami7 <103008905+inami7@users.noreply.github.com>
2022-04-08 15:53:51 +0900

    Initial commit

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,16 @@
+import { VFC } from "react";
+
+export const UserCard: VFC<Props> = () => {
+  return (
+    <div>
+      <dl>
+        <dt>名前</dt>
+        <dd></dd>
+        <dt>メール</dt>
+        <dd></dd>
+        <dt>住所</dt>
+        <dd></dd>
+      </dl>
+    </div>
+  );
+};
